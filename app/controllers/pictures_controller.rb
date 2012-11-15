25734cfdccb6f769d629baa0d9abc0132d664efc
¨Carlos <titofranco@gmail.com>
2012-11-15 16:56:24 -0500

    created scaffold for pictures

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,45 @@
+class PicturesController < ApplicationController
+  # GET /pictures
+  # GET /pictures.json
+  def index
+    @pictures = Picture.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @pictures }
+    end
+  end
+
+  # GET /pictures/1/edit
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  # PUT /pictures/1
+  # PUT /pictures/1.json
+  def update
+    @picture = Picture.find(params[:id])
+
+    respond_to do |format|
+      if @picture.update_attributes(params[:picture])
+        format.html { redirect_to pictures_url, notice: 'Picture was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @picture.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /pictures/1
+  # DELETE /pictures/1.json
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+
+    respond_to do |format|
+      format.html { redirect_to pictures_url }
+      format.json { head :no_content }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Portfolio::Application.routes.draw do
   resources :projects
-
+  resources :pictures, :only => [:index, :edit, :update, :destroy]
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
