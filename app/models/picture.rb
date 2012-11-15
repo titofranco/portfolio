b01f0aca6a716866989058c9fb764a948fafb702
¨Carlos <titofranco@gmail.com>
2012-11-15 16:19:00 -0500

    can now upload several images to a project

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,5 @@
 class Picture < ActiveRecord::Base
-  attr_accessible :caption,, :status, :subtype,
+  attr_accessible :caption, :image, :image_cache, :status, :subtype, :remove_image
+  belongs_to :feature, :polymorphic => true
+  mount_uploader :image, ImageUploader
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ActiveRecord::Base
-  attr_accessible :customer_name, :description, :duration, :status, :tags, :title
+  attr_accessible :customer_name, :description, :duration, :status, :tags, :title, :pictures_attributes
+  has_many :pictures, :as => :feature
+  accepts_nested_attributes_for :pictures, :reject_if => proc { |attributes| attributes['image'].blank? && attributes['image_cache'].blank? }
 end
