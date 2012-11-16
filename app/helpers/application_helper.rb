8c60f13de7e59311a5f5f31ce935260a7969aebd
¨Carlos <titofranco@gmail.com>
2012-11-16 09:50:18 -0500

    added flash message to layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def flash_message
+    messages = ""
+    [:notice, :info, :warning, :error].each {|type|
+      if flash[type]
+        messages += content_tag :div, flash.now[type], :class => "#{type}", :id => "alert"
+      end
+    }
+    messages
+  end
 end
