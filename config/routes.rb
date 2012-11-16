e8e696796d984636818a316b261df10b581ecbe8
¨Carlos <titofranco@gmail.com>
2012-11-16 09:48:27 -0500

    put projects, pictures controllers into scope admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Portfolio::Application.routes.draw do
-  resources :projects
-  resources :pictures, :only => [:index, :edit, :update, :destroy]
+
+  scope "/admin" do
+    resources :projects
+    resources :pictures, :only => [:index, :edit, :update, :destroy]
+  end
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
